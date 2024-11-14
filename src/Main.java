033e360a430d9e97d31f88239d6a034b31b5e217
Hemalatha Chandrasekar <hema.alliswell@gmail.com>
2024-11-14 12:05:15 -0600

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        Robot myRobot = new Robot(); // Create new Robot Object
+        myRobot.move(); // Make the // Make the Robot move
+        myRobot.checkBatteryLevel();// Check the battery level
+        myRobot.stop();// Stop the robot
+        myRobot.changeBattery();// Charge the Robot battery Level
+        myRobot.displayInfo();//Challenge Step
+
+    }
 }
+
--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Robot {
+    int batteryLevel = 100; // Battery level percentage(0 to 100)
+    int speed =0; // current
+    String name="Robo";// Name of the Robo
+    String Color="Red" ;// Color of the Robot
+    // Method 1: Move
+    public void move() {
+        speed=5;
+        System.out.println(name+" is moving at speed "+speed+" mph");
+    }
+    // Method 2: Stop
+    public void stop() {
+        speed = 0;
+        System.out.println(name + " has Stopped ");
+    }
+    // Method 3: Charge Battery
+    public void changeBattery() {
+        batteryLevel=100;
+        System.out.println(name+ "'s battery fully charged.");
+    }
+    // Method 4: Check Battery Level
+    public void checkBatteryLevel(){
+        System.out.println(name+"'s current battery level is "+batteryLevel+" %");
+    }
+    // Challenge Step: DisplayInfo
+
+    public void displayInfo(){
+        System.out.println("Robot Name: "+name);
+        System.out.println("Robot Color: "+ Color);
+        System.out.println("Battery Level: "+batteryLevel+"%");
+        System.out.println("Current Speed: "+speed);
+
+    }
+
 }
